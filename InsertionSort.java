43cc41e9cc8ef0ee09e68f8c1576de996bc7436a
Methulsas <168980299+Methulsas@users.noreply.github.com>
2026-01-21 11:46:26 +0530

    Add files via upload

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,42 +1,12 @@
+public static void insertionSort(int[] arr) {
+    for (int i = 1; i < arr.length; i++) {
+        int key = arr[i];
+        int j = i - 1;
 
-
-public class InsertionSort {
-
-    static void insertionSort(int[] arr) {
-        int n = arr.length;
-
-        for (int i = 1; i < n; i++) {
-            int key = arr[i];
-            int j = i - 1;
-
-            // Shift elements greater than key to one position ahead
-            while (j >= 0 && arr[j] > key) {
-                arr[j + 1] = arr[j];
-                j--;
-            }
-
-            arr[j + 1] = key;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
         }
-    }
-
-    // Display array
-    static void printArray(int[] arr) {
-        for (int x : arr) {
-            System.out.print(x + " ");
-        }
-        System.out.println();
-    }
-
-    public static void main(String[] args) {
-
-        int[] arr = {5, 2, 4, 6, 1, 3};
-
-        System.out.print("Before Sorting: ");
-        printArray(arr);
-
-        insertionSort(arr);
-
-        System.out.print("After Sorting: ");
-        printArray(arr);
+        arr[j + 1] = key;
     }
 }
